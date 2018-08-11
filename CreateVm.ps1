fbdb3094a2947c18f5b976fe23bf0fd0d1446886
Tommi Grönlund <tomgron@calm.fi>
2018-08-11 20:33:14 +0300

    Fixed psremoting installation (hopefully)

--- CreateVm.ps1
+++ CreateVm.ps1
@@ -35,16 +35,22 @@ $SubnetName = "MySubnet"
 $SubnetAddressPrefix = "10.0.0.0/24"
 $VnetAddressPrefix = "10.0.0.0/16"
 
+# Create an inbound network security group rule for port 3389
+$nsgRuleRDP = New-AzureRmNetworkSecurityRuleConfig -Name myNetworkSecurityGroupRuleRDP  -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -Access Allow
+$nsgRulePSR = New-AzureRmNetworkSecurityRuleConfig -Name myNetworkSecurityGroupRuleRDP  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 5986 -Access Allow
+$nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $ResourceGroupName -Location $LocationName -Name myNetworkSecurityGroup -SecurityRules $nsgRuleRDP, $nsgRulePSR
+
+# setup vm networking
 $SingleSubnet = New-AzureRmVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix
 $Vnet = New-AzureRmVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -Location $LocationName -AddressPrefix $VnetAddressPrefix -Subnet $SingleSubnet
 $PIP = New-AzureRmPublicIpAddress -Name $PublicIPAddressName -DomainNameLabel $DNSNameLabel -ResourceGroupName $ResourceGroupName -Location $LocationName -AllocationMethod Dynamic
-$NIC = New-AzureRmNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $LocationName -SubnetId $Vnet.Subnets[0].Id -PublicIpAddressId $PIP.Id
+$NIC = New-AzureRmNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $LocationName -SubnetId $Vnet.Subnets[0].Id -PublicIpAddressId $PIP.Id -NetworkSecurityGroupId $nsg.Id
 
 $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
 
 $VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
 $VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
-$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
+$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id -Primary 
 $VirtualMachine = Set-AzureRmVMSourceImage -VM $VirtualMachine -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' -Skus '2016-Datacenter-with-Containers' -Version latest
 $VirtualMachine = Set-AzureRmVMBootDiagnostics -VM $VirtualMachine -Disable
 
@@ -84,16 +90,20 @@ catch {
     Write-Host ERROR -BackgroundColor Red -ForegroundColor White
 }
 
-# Download install.cmd from gist
-Write "Download install.cmd"
-#Invoke-WebRequest https://gist.githubusercontent.com/tomgron/5309d64c0cc07eb1cac9f048513d9dc3/raw/0a5aacfda61a647708e09f6736ed5c928a1d41f5/install.cmd -OutFile install.cmd
+# Download RemoteInstall.ps1 from gist
+Write "Download latest RemoteInstall.ps1 from GitHub"
+#Invoke-WebRequest https://gist.githubusercontent.com/tomgron/5309d64c0cc07eb1cac9f048513d9dc3/raw/0a5aacfda61a647708e09f6736ed5c928a1d41f5/install.cmd -OutFile .\RemoteInstall.ps1
 
-write "Upload install.cmd to server and start installation - this will take a lot of time..."
-.\UploadScripts.ps1 -ResourceGroupName $ResourceGroupName -VMName $VMName -LocationName $LocationName -ScriptToUpload .\install.cmd -RunFileName "install.cmd" -ScriptExtensionName "installcmd"
+write "Upload RemoteInstall.ps1 to server and start installation - this will take a lot of time..."
+Copy-Item -Path .\RemoteInstall.ps1 -Destination c:\RemoteInstall.ps1 -ToSession $session
+
+# Run the actual command file
+Invoke-Command -Session $session { c:\RemoteInstall.ps1 }
 
 #Optional - shut down the VM to save costs
 Stop-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName -Force
 
 #output necessary info
-Write-Host "IP of server : $addr for RDP connections"
-Write-Host "Username : LocalAdminUser, Password : $VMLocalClearTextPassword"
+Write-Host "DNS of server: $DNSNameLabel.northeurope.cloudapp.azure.com for RDP connections"
+Write-Host "IP of server : $addr"
+Write-Host "Username : $addr\LocalAdminUser, Password : $VMLocalClearTextPassword"
